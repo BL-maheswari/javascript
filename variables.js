6f31f07f44ace054b487ad940217cd794ee51a8b
BL-maheswari <ipperu.maheswari@bridgelabz.com>
2026-04-11 11:19:55 +0530

    [maheswari]::variables file updated

---
+++ variables.js
@@ -0,0 +1,12 @@
+var name = "Maheswari";
+var age = 25;
+
+console.log(name); // Maheswari
+console.log(age);  // 25
+
+let price = 100;
+price = 150; // updated
+
+console.log(price); // 150
+const pi = 3.14;
+console.log(pi); // 3.14
